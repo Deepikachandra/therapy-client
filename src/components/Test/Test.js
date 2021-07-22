056679b4b20e6ae34932b5bd1be245aa202ed916
Deepikachandra <deepikanitraipur@gmail.com>
2021-07-22 14:40:10 +0530

    first commit

---
+++ src/components/Test/Test.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'React';
+
+class Test extends Component{
+ constructor(){
+   super();
+   this.state={
+     data:''
+   }
+ }
+ render(){
+   return(
+     <div>Hi
+     </div>
+   )
+   }
+}
+
+export default Test;
